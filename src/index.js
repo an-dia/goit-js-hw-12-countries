a1f767859f66632441def9becedee7377270346e
AnhelinaDiakova <Gelja4ka@gmail.com>
2020-11-22 19:55:06 +0200

    Добавляет решение 12 модуля

---
+++ src/index.js
@@ -0,0 +1,68 @@
+const debounce = require('lodash.debounce');
+import './styles.scss';
+import { error } from'@pnotify/core';
+import"@pnotify/core/dist/PNotify.css";
+import '@pnotify/core/dist/BrightTheme.css';
+import API from './scripts/api-service'
+import getRefs from './scripts/get-refs'
+import countryMarkupTpl from './templates/country-markup.hbs';
+import countryListTpl from './templates/country-list.hbs'
+
+const refs = getRefs();
+
+refs.input.addEventListener('input', debounce(onSearch, 500));
+
+
+function onSearch(e) {
+  e.preventDefault();
+  clearContainer();
+  const searchQuery = e.target.value.trim();
+
+  API.fetchCountries(searchQuery)
+    .then(renderCountriesCard)
+}
+
+
+
+
+function renderCountriesCard(countries) {
+  const markup = countryMarkupTpl(countries);
+  const list = countryListTpl(countries);
+ 
+  if (countries.length > 10 ) {
+     error({
+      text: "Too many matches found. Pleas enter a more specific query!",
+     type: 'info'
+    })
+    return;
+  }
+
+  if (countries.length >= 2 && countries.length <= 10)
+  {
+    refs.cardContainer.insertAdjacentHTML('beforeend', list);
+    return;
+  }
+
+   if (countries.length === 1)
+   {
+     refs.cardContainer.insertAdjacentHTML('beforeend', markup);
+     return;
+   }
+  
+  if (countries.status === 404) {
+     error({
+      text: "О-о, что-то пошло не так! Не верный ввод. Ваша страна не найдена!!!",
+     type: 'info'
+    })
+    return;
+  }
+}
+
+
+
+function clearContainer() {
+  refs.cardContainer.innerHTML = '';
+  
+}
+
+
---
+++ src/scripts/get-refs.js
@@ -0,0 +1,7 @@
+export default function getRefs (){
+  return {
+    cardContainer: document.querySelector('.js-card-container'),
+    input: document.querySelector('.form-control'),
+    form: document.querySelector('.js-search-form'),
+  }
+}
